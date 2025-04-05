75c9066bbe879ed85a9e6018d2a4be54bc90668d
ruelline <ruelline91@naver.com>
2025-04-05 18:45:33 +0900

    [style] : 프로필 카드 UI 수정 accountScope 값에 맞추어 프로필 공개여부 아이콘 변경되도록 조건추가

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -4,46 +4,75 @@ import Image from "next/image";
 import { useMemberInfo } from "../../features/member/hooks/useMemberInfo";
 import MyPageMenu from "./components/mypageMenu";
 
+import { useRouter } from "next/navigation";
+import { ChevronLeft, Globe, Lock, User } from "lucide-react";
+
 export default function MyPage() {
+  const Router = useRouter();
   const { data: member, isLoading } = useMemberInfo();
 
   if (isLoading || !member) return <div>로딩 중...</div>;
-
+  const handleBack = () => {
+    Router.push("/");
+  };
   const profile = member.profileInfo;
   return (
     <>
       <div className="mx-auto px-4 py-8">
-        {/* 닉네임 + 프로필 */}
-        <div className="flex items-center justify-between">
+        <div
+          className="flex my-4 -ml-2
+        "
+        >
+          <button onClick={handleBack}>
+            <ChevronLeft />{" "}
+          </button>
+        </div>
+        <div className="flex items-center gap-4">
           <div className="flex items-center gap-4">
             <Image
               src={profile.profileImageUrl || "/default-avatar.png"}
               alt="프로필 이미지"
-              width={72}
-              height={72}
+              width={100}
+              height={100}
               className="rounded-full object-cover"
             />
-            <div className="text-xl font-semibold">{profile.nickname}</div>
           </div>
-        </div>
 
-        {/* 통계 */}
-        <div className="flex justify-around mt-6 text-center">
-          <div>
-            <div className="text-lg font-bold">{profile.followerCount}</div>
-            <div className="text-sm text-gray-500">팔로워</div>
-          </div>
-          <div>
-            <div className="text-lg font-bold">{profile.followingCount}</div>
-            <div className="text-sm text-gray-500">팔로잉</div>
-          </div>
-          <div>
-            <div className="text-lg font-bold">{profile.travelDiaryCount}</div>
-            <div className="text-sm text-gray-500">여행일지</div>
-          </div>
-          <div>
-            <div className="text-lg font-bold">{profile.placesCount}</div>
-            <div className="text-sm text-gray-500">플레이스</div>
+          <div className="flex-1">
+            <div className="flex items-center mb-4 gap-1">
+              <h2 className="text-xl font-bold ">{profile.nickname}</h2>
+              <div className="text-gray-500">
+                {profile.accountScope === "PUBLIC" && <Globe className="w-4" />}
+                {profile.accountScope === "FRIENDS" && <User className="w-4" />}
+                {profile.accountScope === "PRIVATE" && <Lock className="w-4" />}
+              </div>
+            </div>
+            <dl className="flex justify-between ">
+              <div className="flex items-center gap-2">
+                <dt className=" text-gray-500">팔로워</dt>
+                <dd className=" font-semibold">
+                  {String(profile.followerCount)}
+                </dd>
+              </div>
+              <div className="flex items-center gap-2">
+                <dt className=" text-gray-500">팔로잉</dt>
+                <dd className=" font-semibold">
+                  {String(profile.followingCount)}
+                </dd>
+              </div>
+              <div className="flex items-center gap-2">
+                <dt className=" text-gray-500">여행일지</dt>
+                <dd className=" font-semibold">
+                  {String(profile.travelDiaryCount)}
+                </dd>
+              </div>
+              <div className="flex items-center gap-2">
+                <dt className=" text-gray-500">플레이스</dt>
+                <dd className=" font-semibold">
+                  {String(profile.placesCount)}
+                </dd>
+              </div>
+            </dl>
           </div>
         </div>
       </div>
