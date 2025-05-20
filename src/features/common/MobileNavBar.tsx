312bbace3c39dc2ce96f4d2b3e027282d43ed674
ruelline <ruelline91@naver.com>
2025-05-20 13:58:26 +0900

    [style] : 모바일 네비게이션 하단여백이 없어 실제 모바일에서 사용시 버튼을 누르기 어려웠던 이슈해결
    - pb-6 = 24px 정도로 적용하여 floating 되도록 적용
    - 주조색을 활용한 color 적용

--- src/features/common/MobileNavBar.tsx
+++ src/features/common/MobileNavBar.tsx
@@ -15,7 +15,7 @@ export default function MobileNavBar() {
   const pathname = usePathname();
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t shadow z-50 md:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t shadow z-50 md:hidden pb-8">
       <ul className="flex justify-around items-center">
         {tabs.map((tab) => {
           const isActive =
@@ -29,19 +29,19 @@ export default function MobileNavBar() {
               <Link href={tab.href} className="flex flex-col items-center">
                 <tab.icon
                   className={`w-6 h-6 mb-0.5 ${
-                    isActive ? "text-black" : "text-gray-400"
+                    isActive ? "text-primary-main" : "text-gray-400"
                   }`}
                 />
                 <span
                   className={
-                    isActive ? "text-black font-medium" : "text-gray-400"
+                    isActive ? "text-primary-main font-medium" : "text-gray-400"
                   }
                 >
                   {tab.label}
                 </span>
               </Link>
               {isActive && (
-                <span className="absolute -bottom-0 left-1/2 transform -translate-x-1/2 w-4 h-[3px] bg-black rounded-full" />
+                <span className="absolute -bottom-0 left-1/2 transform -translate-x-1/2 w-4 h-[3px] bg-primary-light rounded-full" />
               )}
             </li>
           );
