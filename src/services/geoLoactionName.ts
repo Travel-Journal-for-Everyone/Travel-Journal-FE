92a50ce8e68efeffdccb565601f9e539a221a289
ruelline <ruelline91@naver.com>
2025-06-11 16:31:33 +0900

    [feat] : getLocationName 비동기 함수로 변경

--- src/services/geoLoactionName.ts
+++ src/services/geoLoactionName.ts
@@ -23,3 +23,32 @@ export function getLocationName(
     }
   });
 }
+
+export function getLocationNameAsync(
+  lat: number,
+  lng: number
+): Promise<string> {
+  return new Promise((resolve) => {
+    getLocationName(lat, lng, (address) => resolve(address));
+  });
+}
+
+export function searchPlace(
+  keyword: string,
+  callback: (lat: number, lng: number) => void
+) {
+  if (!window.kakao || !window.kakao.maps || !window.kakao.maps.services) {
+    console.warn("Kakao Maps 스크립트가 아직 로드되지 않았습니다.");
+    return;
+  }
+
+  const ps = new window.kakao.maps.services.Places();
+  ps.keywordSearch(keyword, (data, status) => {
+    if (status === window.kakao.maps.services.Status.OK) {
+      const firstResult = data[0];
+      callback(parseFloat(firstResult.y), parseFloat(firstResult.x));
+    } else {
+      console.warn("위치 검색 실패:", status);
+    }
+  });
+}
