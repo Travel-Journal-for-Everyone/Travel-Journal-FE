dc66ab7043584e4187aac479e51245a8d68e2b77
ruelline <ruelline91@naver.com>
2025-07-30 21:13:23 +0900

    [feat] : 목업데이터 삭제 및 리스트에 링크 적용

--- src/features/explore/components/exploreCard.tsx
+++ src/features/explore/components/exploreCard.tsx
@@ -33,38 +33,22 @@ export default function ExploreCard({
     <div className="rounded-xl overflow-hidden shadow-sm border bg-white">
       <div className="relative aspect-square text-overlay-gradient">
         <div className="absolute z-10 flex items-center gap-2 w-full  p-4">
-          <Image
-            src={profileImageUrl}
-            alt={nickname}
-            width={32}
-            height={32}
-            className="rounded-full object-cover"
-          />
+          <Image src={profileImageUrl} alt={nickname} width={32} height={32} className="rounded-full object-cover" />
           <span className="font-medium text-white drop-shadow">{nickname}</span>
         </div>
 
-        <Image
-          src={thumbnailUrl}
-          alt="journal thumbnail"
-          fill
-          className="object-cover z-0"
-        />
+        <Image src={thumbnailUrl} alt="journal thumbnail" fill className="object-cover z-0" />
 
         <div className="flex flex-wrap gap-2 absolute z-10 bottom-0 left-0 p-4">
           {hashTag.slice(0, 3).map((tag, idx) => (
-            <span
-              key={idx}
-              className="text-xs bg-white text-primary-main px-2 py-1 rounded-full"
-            >
+            <span key={idx} className="text-xs bg-white text-primary-main px-2 py-1 rounded-full">
               #{tag}
             </span>
           ))}
         </div>
       </div>
       <div className="p-4 pt-3">
-        <h3 className="text-base font-semibold text-gray-900 mb-1 line-clamp-2">
-          {title}
-        </h3>
+        <h3 className="text-base font-semibold text-gray-900 mb-1 line-clamp-2">{title}</h3>
 
         <p className="text-xs text-gray-500 mb-1">
           {startDate} ~ {endDate}
